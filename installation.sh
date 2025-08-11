819dc56d80ebff26a21fd3ca7bbf1b6d30087ee3
Dilip <dilip2017.voleti@gmail.com>
2025-08-11 16:49:08 +0530

    my commit

--- installation.sh
+++ installation.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+
 es=$(echo $?)
-if [ $es -ne 0 ]
-then exit 1 
-fi 
+
+
 ydjddhdj
 if [ $es -ne 0 ]
 then exit 1 
 fi 
+
 echo "dilip"
 if [ $es -ne 0 ]
 then exit 1 
-fi 
+fi
+
 echo " hii"
 if [ $es -ne 0 ]
 then exit 1 
-fi  
+fi
+
 hjddjkd
 if [ $es -ne 0 ]
 then exit 1 
-fi 
+fi
+ 
 echo "hello"
 if [ $es -ne 0 ]
 then exit 1 
