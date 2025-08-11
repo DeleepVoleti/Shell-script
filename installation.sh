61e75d98b91e4f91b559841dc984ec47fa2e3479
Dilip <dilip2017.voleti@gmail.com>
2025-08-11 16:24:53 +0530

    my commit

--- installation.sh
+++ installation.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-uid=$(id -u)
-
-if [ $uid -ne 0 ]
-then 
-    echo " please get root acess to run this code "
-    exit 1
-fi
-
-dnf install mysql -y
-
-echo " hi dilip" 
+ydjddhdj
+echo "dilip"
+echo " hii" 
+hjddjkd
+echo "hello'
