8db1ba47f68fb40d9b06dedf1b5be65efceca559
Dilip <dilip2017.voleti@gmail.com>
2025-08-12 11:24:56 +0530

    new-commit

--- functions.sh
+++ functions.sh
@@ -5,11 +5,12 @@
 if [ $1 -ne 0 ]
 then echo "run this code with root access"
 exit 1 
+else echo "you r root user "
 fi
  }
 
 validate(){
-if [$1 -ne 0]
+if [ $1 -ne 0 ]
 then echo "installation is failure"
 else echo "success or already installed" 
 fi
